1662d75970074c77c260efddca5311214d95a097
theXtroyer1221 <jaddou2005@gmail.com>
2020-02-15 12:17:53 +0000

    changes to uppgift 9

--- main.py
+++ main.py
@@ -3,18 +3,15 @@ import requests
 
 class app:
   def convert_USD_SEK(self, number):
-    
+    #Detta function tar in hur mycket USD och kollar hur mycket SEK det är i nätet, sedan räcknar det ut priset     
     r = requests.get('https://free.currconv.com/api/v7/convert?apiKey=do-not-use-this-key&q=USD_SEK&compact=ultra')
-
     data = r.json()
-
-print(data["USD_SEK"])
-    #Detta function tar in hur mycket USD och kollar hur mycket SEK det är i nätet, sedan räcknar det ut priset 
-    mulnumber = float(number) / 100 
-    print(f"{number}cm är {mulnumber}m")
+    data["USD_SEK"].strip(5)
+    SEK = data["USD_SEK"] * number
+    print(f"{number}USD är {SEK}SEK")
 
 print("Genom att dela med hundra får vi cm till m")
 #input från användaren
 USD = input("hur mycket USD?: ")
 app.convert_USD_SEK(app, USD)
- 
+
